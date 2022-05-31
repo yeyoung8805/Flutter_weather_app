f05dc27ab74b1263b1a6555673669ac021fd8c80
yeyoung8805 <yyk8805@naver.com>
2022-05-31 17:51:33 +0900

    add getLocation method and print geolocation

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({key});
 
   // This widget is the root of your application.
   @override
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Loading(),
+      home: const Loading(),
     );
   }
 }
--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Loading extends StatefulWidget {
   //stful 입력하고 자동완성 나오면 엔터
@@ -9,13 +10,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getLocation() async{
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
-          child: Text(
+          onPressed: () {
+            getLocation();
+          },
+          child: const Text(
             'Get my location',
             style: TextStyle(color: Colors.white),
           ),
